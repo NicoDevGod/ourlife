49c4fbcad4ab9e2cba44dab45eab802104cfe6bd
NicoDevGod <nicoange421@gmail.com>
2023-12-08 18:24:53 -0300

    First commit

--- src/components/HorizontalTimeline.jsx
+++ src/components/HorizontalTimeline.jsx
@@ -19,6 +19,60 @@ const items = [
     cardTitle: "Event 3",
     cardSubtitle: "Event 3 Subtitle",
     cardDetailedText: "This is the third event on the timeline.",
+  },
+  {
+    title: "January 2022",
+    cardTitle: "Event 1",
+    cardSubtitle: "Event 1 Subtitle",
+    cardDetailedText: "This is the first event on the timeline.",
+  },
+  {
+    title: "February 2022",
+    cardTitle: "Event 2",
+    cardSubtitle: "Event 2 Subtitle",
+    cardDetailedText: "This is the second event on the timeline.",
+  },
+  {
+    title: "March 2022",
+    cardTitle: "Event 3",
+    cardSubtitle: "Event 3 Subtitle",
+    cardDetailedText: "This is the third event on the timeline.",
+  },
+  {
+    title: "January 2022",
+    cardTitle: "Event 1",
+    cardSubtitle: "Event 1 Subtitle",
+    cardDetailedText: "This is the first event on the timeline.",
+  },
+  {
+    title: "February 2022",
+    cardTitle: "Event 2",
+    cardSubtitle: "Event 2 Subtitle",
+    cardDetailedText: "This is the second event on the timeline.",
+  },
+  {
+    title: "March 2022",
+    cardTitle: "Event 3",
+    cardSubtitle: "Event 3 Subtitle",
+    cardDetailedText: "This is the third event on the timeline.",
+  },
+  {
+    title: "January 2022",
+    cardTitle: "Event 1",
+    cardSubtitle: "Event 1 Subtitle",
+    cardDetailedText: "This is the first event on the timeline.",
+  },
+  {
+    title: "February 2022",
+    cardTitle: "Event 2",
+    cardSubtitle: "Event 2 Subtitle",
+    cardDetailedText: "This is the second event on the timeline.",
+  },
+  {
+    title: "March 2022",
+    cardTitle: "Event 3",
+    cardSubtitle: "Event 3 Subtitle",
+    cardDetailedText: "This is the third event on the timeline.",
   }
 ];
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import Timeline from './components/TimeLine.jsx'
+
 import HorizontalTimeline from './components/HorizontalTimeline.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <Timeline />
     <HorizontalTimeline />
   </React.StrictMode>,
 )
